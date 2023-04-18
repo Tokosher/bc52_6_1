5e16d67459dc05dab1124a6669b20bc6539606ab
Illia <illia.tkach@isoftbet.com>
2023-04-18 11:06:46 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// const button = document.querySelector('button')
+// const input = document.querySelector('input')
+//
+// button.addEventListener('click', () => {
+//     const value = input.value;
+//     if (value === '') {
+//         return alert('Введіть значення в поле')
+//     }
+//     alert(value);
+// })
+
+// const button = document.querySelector('button')
+// const input = document.querySelector('input')
+// // mousedown, mouseup, click
+// button.addEventListener('click', () => {
+//     const value = input.value;
+//     if (value === '') {
+//         return alert('Введіть значення в поле')
+//     }
+//     alert(value);
+// })
+
+// const input = document.querySelector('input')
+// // keydown, keyup
+// document.addEventListener('keydown', event => {
+//     if (event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode === 32) {
+//         const keyValue = event.key;
+//         input.value += keyValue;
+//     }
+// })
+
+// const button = document.querySelector('button')
+// const input = document.querySelector('input')
+//
+// button.addEventListener('click', onButtonClick)
+//
+// input.addEventListener('keydown', event => {
+//     if (event.key === 'Enter') {
+//         onButtonClick();
+//     }
+// })
+//
+// function onButtonClick () {
+//     const value = input.value;
+//     if (value === '') {
+//         return alert('Введіть значення в поле')
+//     }
+//     alert(value);
+// }
